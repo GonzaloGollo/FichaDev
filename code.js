e67565bdd95edc5dc608013570afcac41124d833
GonzaloGollo <gonzalogollo@gmail>
2022-11-10 01:29:19 -0300

    Revision comentarios

--- code.js
+++ code.js
@@ -36,51 +36,52 @@ cambiarTema.addEventListener("click", alternarColorTema);
 /* --------------------------- NO TOCAR HASTA ACÁ --------------------------- */
 
 function obtenerDatosDelUsuario() {
-/* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
-    
-//////////////////////// Validacion 
+  /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
+   
+  /* --------------- Validacion  --------------- */
     let consultaNombre;
     let caracterNN = "!@#$%^&*()+=-[·]\\\';,./{}|\":<>?01234567890";
 
     function obtenerNombreValido(){
         consultaNombre = prompt("Ingresa tu nombre");
-        if((consultaNombre.length < 3) || (consultaNombre == null) || (consultaNombre == undefined) || (consultaNombre == "    ")){
-            alert("Por favor ingrese un nombre valido. El texto ingresado es muy corto o está vacio.");
+        if (( consultaNombre.length < 2 ) || ( consultaNombre === null ) || ( consultaNombre === undefined ) || ( consultaNombre.includes("  ")== true )) {
+            alert( "Por favor ingrese un nombre valido. El texto ingresado es muy corto o está vacio." );
             obtenerNombreValido();
-        } else if (consultaNombre.length>=3){
+        } else if ( consultaNombre.length>=3 ){
             for ( let i = 0; i < consultaNombre.length; i++ ){ 
-                if((caracterNN.indexOf(consultaNombre.charAt(i))!= -1) ){
+                if (( caracterNN.indexOf( consultaNombre.charAt(i) )!= -1) ){
                     alert("Por favor ingrese un nombre valido. Debe ingresar solo letras.");
                     obtenerNombreValido();
                 };
-            }   
+            };
         } else {
         return consultaNombre;
         };
     };
     obtenerNombreValido();
 
-///////////////////////
+  /* ---------------  AÑO  --------------- */
+
     const anioActual = 2022;
     let consultaEdad; 
     function obtenerEdadValida(){
-        consultaEdad = (anioActual - (int(prompt("Ingrese el año en que naciste"))));
-        if (((consultaEdad) > 100 ) || ((consultaEdad) < 18) ||(isNaN(consultaEdad)) ){
+        consultaEdad = (anioActual - ( parseInt( prompt( "Ingrese el año en que naciste" ) ) ));
+        if ( ( (consultaEdad) > 100 ) || ( (consultaEdad) < 18 ) ||( isNaN(consultaEdad) ) ){
             alert("Por favor ingrese un año válido");
             obtenerEdadValida();
         };
         return consultaEdad;
     };    
     obtenerEdadValida();
-/////////////////////////////
+
 
 /* const consultaCiudad = prompt("Ingresa la ciudad en que vives");*/
 
     let consultaCiudad;
     function obtenerCiudadValida(){
-        consultaCiudad = (prompt("Ingresa la ciudad en que vives"));
-        if ((consultaCiudad.length <= 3) || (consultaCiudad === null) || (consultaCiudad === undefined) || (consultaCiudad.length == -1)){
-            alert("Por favor ingrese una ciudad válida");
+        consultaCiudad = prompt("Ingresa la ciudad en que vives");
+        if ( (consultaCiudad.length <= 3) || (consultaCiudad === null) || (consultaCiudad === undefined) || (consultaCiudad.length == -1) ){
+            alert( "Por favor ingrese una ciudad válida" );
             obtenerCiudadValida();
         };
         return consultaCiudad;
@@ -88,11 +89,8 @@ function obtenerDatosDelUsuario() {
     obtenerCiudadValida();
 
 /*---------------------------------------*/
-
-
     const consultaInteresPorJs = confirm("¿Te interesa Javascript?");
 /*---------------------------------------*/
-
     datosPersona.nombre= consultaNombre;
     datosPersona.edad= consultaEdad;
     datosPersona.ciudad= consultaCiudad;
@@ -107,15 +105,14 @@ function renderizarDatosUsuario() {
     document.querySelector("#edad").innerHTML= `${datosPersona.edad}` ;
     document.querySelector("#ciudad").innerHTML= `${datosPersona.ciudad}` ;
     document.querySelector("#javascript").innerHTML= `${datosPersona.interesPorJs? "Sí":"No"}` ;
-}
+};
 
 
 function recorrerListadoYRenderizarTarjetas() {
-  /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
+/* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
 /*-----------------  OK   ---------- CAMBIAR Y Q UNA VEZ Q HAGA CLICK SOLO SE VISUALICE< Y EL NUEVO CLICK NO HAGA NADA ---------*/
-let tarjeta;
+    let tarjeta;
     let seccionTarjetas = document.querySelector("#fila");
-
     if ( !seccionTarjetas.classList.contains("tarjetasVistas") ) {
         seccionTarjetas.classList.add("tarjetasVistas");
         listado.forEach(dato => {
@@ -126,30 +123,20 @@ let tarjeta;
             seccionTarjetas.innerHTML +=tarjeta;
             materiasBtn.removeEventListener("click", recorrerListadoYRenderizarTarjetas);
         });
-    } 
-    // else {
-    //     seccionTarjetas.classList.remove("tarjetasVistas");
-    //     seccionTarjetas.innerHTML=``;
-    // } 
+    };
 };
 
 function alternarColorTema() {
 /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
-/*-----------------  OK   -------------------*/
-    const sitio =  document.querySelector('#sitio');
+const sitio =  document.querySelector('#sitio');
     sitio.classList.contains("dark")? sitio.classList.remove("dark") : sitio.classList.add("dark");
 };
 
 /* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
-/*-----------------  OK   -------------------*/
 const txtSobreMi = document.querySelector('#sobre-mi');
 function visualizarSobreMi(){ 
     window.addEventListener("keydown", (e) => {
-        if (e.key === "f" || e.key === "F") {
-            // txtSobreMi.classList.contains("oculto")? txtSobreMi.classList.remove("oculto"): txtSobreMi.classList.add("oculto");
-            txtSobreMi.classList.remove("oculto");
-        };
+        (e.key === "f" || e.key === "F")? txtSobreMi.classList.remove("oculto"):"";
     });
 };
-
 visualizarSobreMi();
